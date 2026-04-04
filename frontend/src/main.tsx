b1583c830c1c7b285364cf0367f200f7ad7f1c55
angelahanasan <hanasanangela@gmail.com>
2026-04-04 19:37:18 -0400

    Add Login Form HTML+CSS

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import LoginForm from './components/LoginForm.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LoginForm />
   </StrictMode>,
 )
