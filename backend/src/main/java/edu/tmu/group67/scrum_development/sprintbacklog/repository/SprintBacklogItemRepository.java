fed8913a7d0a116c28d331d64c1a8388e3cf4fa4
Emaan <emaan@gmail.com>
2026-04-09 01:37:21 -0400

    e6

--- backend/src/main/java/edu/tmu/group67/scrum_development/sprintbacklog/repository/SprintBacklogItemRepository.java
+++ backend/src/main/java/edu/tmu/group67/scrum_development/sprintbacklog/repository/SprintBacklogItemRepository.java
@@ -1,10 +1,12 @@
 package edu.tmu.group67.scrum_development.sprintbacklog.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import edu.tmu.group67.scrum_development.sprintbacklog.model.entity.SprintBacklogItemEntity;
 
 @Repository
 public interface SprintBacklogItemRepository extends JpaRepository<SprintBacklogItemEntity, Long> {
-    
+    List<SprintBacklogItemEntity> findBySprintId(Long sprintId);
 }
